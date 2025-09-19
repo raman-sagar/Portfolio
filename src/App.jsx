3a535fc3d8ea2e3b735c318d45a6f9ac3446ef28
raman-sagar <ramansagar938923@gmail.com>
2025-09-19 13:46:27 +0530

    floating cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useRef } from "react";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
@@ -7,11 +7,43 @@ import { Experiance } from "./components/Experiance";
 import { Footer } from "./components/Footer";
 import { Contact } from "./components/Contact";
 import { Toaster } from "react-hot-toast";
+import "./App.css";
 
 function App() {
+  const cursor = useRef(null);
+  const pointer = useRef(null);
+  {
+    document.addEventListener("mousemove", (e) => {
+      const x = e.clientX - 25;
+      const y = e.clientY - 25;
+      cursor.current.style.left = x + "px";
+      cursor.current.style.top = y + "px";
+    });
+
+    document.addEventListener("mousedown", (e) => {
+      pointer.current.classList.add("scale");
+    });
+
+    document.addEventListener("mouseup", (e) => {
+      pointer.current.classList.remove("scale");
+    });
+
+    // Show and Disappear Cursor
+
+    document.addEventListener("mouseenter", () => {
+      cursor.current.style.opacity = "1";
+    });
+
+    document.addEventListener("mouseleave", () => {
+      cursor.current.style.opacity = "0";
+    });
+  }
   return (
     <>
-      <div className="select-none">
+      <div id="cursor" ref={cursor}>
+        <div className="pointer" ref={pointer}></div>
+      </div>
+      <div className="select-text">
         <Navbar />
         <Home />
         <About />
